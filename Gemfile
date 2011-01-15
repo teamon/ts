9a54131d092d35b8cf06cbe22175eca874286834
Tymon Tobolski <i@teamon.eu>
2011-01-15 21:25:46 +0100

    Base stuff

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source :gemcutter
+
+gem "rack", "1.2.1"
+gem 'sinatra', '1.0'
+gem "thin"
+gem "haml", "3.0.18"
+
+gem 'nokogiri', '1.4.3.1'
+
+# gem "rack-google_analytics", '1.0.1', :require => "rack/google_analytics"
---
+++ ts.rb
@@ -0,0 +1,42 @@
+require "sinatra"
+require "haml"
+require "nokogiri"
+
+class TS
+  class << self
+    CACHE_FILE = "/tmp/cache.html"
+    DATA_URL = "http://kefirnet.ath.cx/sygnaly/?pokaz"
+    def sample(n)
+      load
+      @doc = Nokogiri::HTML(@data)
+      
+      pp @doc.css("div.question").to_a.sample(n).map {|q0|
+        {
+          :text => q0.css("div.questionText").text.strip,
+          :answers => q0.css("ul li").map {|q1|
+            {
+              :text => q1.text.strip,
+              :correct => !!q1.css("input").attr("checked")
+            }
+          }
+        }
+      }
+    end
+  
+  
+    def load
+      puts "Checking #{CACHE_FILE}..."
+      @data = File.read(CACHE_FILE)
+      puts "Using cache file"
+    rescue
+      puts "Cache not found. Loading #{DATA_URL}..."
+      @data = open(DATA_URL).read
+      File.open(CACHE_FILE, "w"){|f| f.write @data }
+    end
+  end
+end
+
+get "/" do
+  @questions = TS.sample(10)
+  haml :index
+end
